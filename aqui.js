8544b36371b60d9f333bb7766c6b8f5e40c8749b
christineeee <41602221+christineeee@users.noreply.github.com>
2018-07-24 10:56:32 +0800

    Update aqui.js

--- aqui.js
+++ aqui.js
@@ -1,1 +1,66 @@
-
+[
+  {
+    "id": 145,
+    "name": "Grapes Farm La Union",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/tinn/master/New%20folder/1.jpg",
+    "price": 250,
+    "priceCurrency": "₱",
+    "lat":16.461085,
+    "lng": 120.333895
+  },
+  {
+    "id": 148,
+    "name": "Pugad La union",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/tinn/master/New%20folder/2.jpg",
+    "price": 200,
+    "priceCurrency": "₱",
+    "lat": 16.336737,
+    "lng": 120.478875
+  },
+  {
+    "id": 201,
+    "name": "Luna Launion",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/tinn/master/New%20folder/3.jpg",
+    "price": 200,
+    "priceCurrency": "₱",
+    "lat": 16.853432,
+    "lng": 120.376654
+  },
+  {
+    "id": 205,
+    "name": "Macho Temple La Union",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/tinn/master/New%20folder/4.jpg",
+    "price": 300,
+    "priceCurrency": "₱",
+    "lat": 16.621197,
+    "lng": 120.317470
+  },
+  {
+    "id": 217,
+    "name": "Light House La union",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/tinn/master/New%20folder/6.jpg",
+    "price": 800,
+    "priceCurrency": "₱",
+    "lat": 16.614851,
+    "lng": 120.280511
+  },
+  {
+    "id": 280,
+    "name": "Thunderbird La union",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/tinn/master/New%20folder/7.jpg",
+    "price": 900,
+    "priceCurrency": "₱",
+    "lat": 16.608991,
+    "lng": 120.286519
+  },
+  {
+    "id": 910,
+    "name": "Bahay na Bato La union",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/tinn/master/New%20folder/8.jpg",
+    "price": 100,
+    "priceCurrency": "₱",
+    "lat": 16.847194,
+    "lng": 120.351700
+  }
+  
+]
